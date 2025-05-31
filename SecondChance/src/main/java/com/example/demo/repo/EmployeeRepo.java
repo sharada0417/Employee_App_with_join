4bea273a3b7860db343852e3cbb193ce466308a1
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-31 22:31:36 +0530

    create EmployeeRepo

---
+++ SecondChance/src/main/java/com/example/demo/repo/EmployeeRepo.java
@@ -0,0 +1,20 @@
+package com.example.demo.repo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Employee;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<Employee,String>{
+
+	//find the employees with the salary range 70000 to 90000
+	@Query("select e from Employee e where e.salary between ?1 and 2?")
+	public List<Employee> findSalaryRange(int a,int b);
+	
+	@Query("select e from Employee e where e.department.id = ?1")
+	public List<Employee> findByDept(int did);
+	}
