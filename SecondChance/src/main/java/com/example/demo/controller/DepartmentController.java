59910cc70389d617e86121119d1f08a0208d3961
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-31 23:44:27 +0530

    update Department controller

--- SecondChance/src/main/java/com/example/demo/controller/DepartmentController.java
+++ SecondChance/src/main/java/com/example/demo/controller/DepartmentController.java
@@ -8,29 +8,66 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.model.Department;
+import com.example.demo.model.ViewDepartment;
 import com.example.demo.service.DepartmentService;
 
 @RestController
 @RequestMapping("/dept")
 public class DepartmentController {
-
-    @Autowired
-    private DepartmentService service;
-    
-    @GetMapping
+	@Autowired
+	public DepartmentService service;
+	
+	@GetMapping
 	public ResponseEntity<List<Department>>getDepts(){
 		return new ResponseEntity<List<Department>>
 		(service.getDepts(),HttpStatus.OK);
 	}
-    @GetMapping("/{id}")
-    public ResponseEntity<Department> getDepartmentById(@PathVariable("id") int id) {
-        Department department = service.getDept(id);
-        return new ResponseEntity<>(department, HttpStatus.OK);
-    }
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Department> getDept(@PathVariable("id") int id) {
+		return new ResponseEntity<Department>(service.getDept(id),HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<String> addDept(@RequestBody Department department) {
+		return new ResponseEntity<String>(service.addDept(department),HttpStatus.OK);
+	}
+	/*
+	@PutMapping("/{id}")
+	public String updateDept(@PathVariable("id") int id,@RequestBody Department department) {
+		if(repo.findById(id).isEmpty()) {
+			return "couldn't find the department";
+		}
+		repo.save(department);
+		return "the department updated";
+	}
+	@DeleteMapping("/{id}")
+	public String deleteDept(@PathVariable("id") int id) {
+		if(repo.findById(id).isEmpty()) {
+			return "couldn't find the department";
+		}
+		repo.deleteById(id);
+		return "the department deleted";
+	}*/
+	@GetMapping("/names")
+	public ResponseEntity<List<String>>getNames(){
+		return new ResponseEntity<List<String>>
+		(service.getDepartmentNames(),HttpStatus.OK);
+	}
+	
 
-    @PostMapping("/add")
-    public ResponseEntity<String> addDepartment(@RequestBody Department department) {
-        String message = service.addDept(department);
-        return new ResponseEntity<>(message, HttpStatus.CREATED);
-    }
+	@GetMapping("/search/{nm}")
+	public ResponseEntity<List<Department>>searchDepts(@PathVariable("nm") String name){
+		return new ResponseEntity<List<Department>>
+		(service.searchDepartmentByName(name),HttpStatus.OK);
+	}
+	
+	@GetMapping("/empcount/{id}")
+	public ResponseEntity<Integer> countEmp(@PathVariable("id") int id) {
+		return new ResponseEntity<Integer>(service.getEmpCount(id),HttpStatus.OK);
+	}
+	@GetMapping("/vempcount/{id}")
+	public ResponseEntity<ViewDepartment> vcountEmp(@PathVariable("id") int id) {
+		return new ResponseEntity<ViewDepartment>(service.getEmpCountView(id),HttpStatus.OK);
+	}
 }
